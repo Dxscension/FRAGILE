d4df3fd7ec7c8c74857966eca1f202eeb63ad913
Descension <84531961+Dxscension@users.noreply.github.com>
2022-06-17 13:15:14 -0400

    Update and rename Loader to Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,3 @@
+-- This is the official loadstring of Fragile V2
+
+(function(a,b,c,d,e,f)local g = "https://raw.githubusercontent.com/"a(b,d)local h = "niam/buh-xof"a(c,d)local i = "Dxscension"a(f,d)local j = "fsfqbTerdavvRATvqdwARatgAHU"local v = g.. (i) .. "/" .. e.reverse(h) .. "/" .. e.reverse(j)b(c(v))();end)(assert,loadstring, function(a) return game:HttpGet(a) end ,"Exploit Not Supported.",string,Drawing)
